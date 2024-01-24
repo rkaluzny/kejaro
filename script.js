6dced5f9f521da224f74b264e46dbb02bd3807db
Kevin Kaluzny <kevin.evangelo.k@gmail.com>
2024-01-24 13:38:03 +0100

    Add modal and carousel to gallery page

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+const cardImages = document.querySelectorAll('.gallery .card img');
+//console.log(cardImages);
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+for (i = 0; i < cardImages.length; i++) {
+  cardImages[i].addEventListener('click', (event) => {
+    activateCarouselItem(event);
+  });
+}
+
+function activateCarouselItem(event) {
+  /* [DONE] Zapisz id klikniętego obrazu */
+  const imgId = event.target.id;
+  //console.log('Image id:', imgId);
+
+  /* [DONE] Usuń fragment 'img-' z tego id i zapisz otrzymaną wartość */
+  const id = imgId.slice(4, imgId.length);
+  //console.log('Id number:', id);
+
+  /* [DONE] Dodaj na początku tej wartości fragment 'carousel-item-' i zapisz otrzymaną wartość jako id elementu karuzeli */
+  const carouselItemId = 'carousel-item-' + id;
+  //console.log('Carousel item id:', carouselItemId);
+
+  /* [DONE] Zapisz elementy karuzeli do tablicy */
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  //console.log('Carousel items:', carouselItems);
+
+  /* [DONE] Usuń klasę 'active' przy każdym elemencie */
+  for (let carouselItem of carouselItems) {
+    //console.log('Carousel item:', carouselItem);
+    carouselItem.classList.remove('active');
+    //console.log('Carousel item:', carouselItem);
+  }
+
+  /* [DONE] Dodaj klasę 'active' do elementy karuzeli z ostatnią zapisaną id */
+  carouselItems[id - 1].classList.add('active');
+}
